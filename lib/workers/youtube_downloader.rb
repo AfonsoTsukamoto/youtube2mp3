dcdbaad9615aff80b69c870e4b0baaa8af759d48
Ricardo Otero <oterosantos@gmail.com>
2014-10-18 21:05:38 +0100

    Don't retry the failed jobs

--- lib/workers/youtube_downloader.rb
+++ lib/workers/youtube_downloader.rb
@@ -1,10 +1,12 @@
 class YoutubeDownloader
   include MediaConverter
   include Sidekiq::Worker
-  sidekiq_options queue: :downloads
+
+  sidekiq_options queue: :downloads, retry: false
 
   def perform(media)
     media = JSON.parse(media)
+
     YoutubeDl.download(media['youtube_url'], media['file_name'])
   end
 end
